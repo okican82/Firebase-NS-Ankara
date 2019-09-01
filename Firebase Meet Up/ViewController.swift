65c25b6729a6cd92dde7bc10adeb68588f3906b7
okay yıldırım <okay.yildirim@gmail.com>
2019-09-01 20:42:51 +0300

    initialize

---
+++ Firebase Meet Up/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Firebase Meet Up
+//
+//  Created by Enocta on 1.09.2019.
+//  Copyright © 2019 Enocta. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+

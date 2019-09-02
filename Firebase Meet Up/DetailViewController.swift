c472d1d8890724cab6bc4c744dea4328a20decf6
okay yıldırım <okay.yildirim@gmail.com>
2019-09-02 08:29:45 +0300

    email login ok

---
+++ Firebase Meet Up/DetailViewController.swift
@@ -0,0 +1,30 @@
+//
+//  DetailViewController.swift
+//  Firebase Meet Up
+//
+//  Created by Enocta on 1.09.2019.
+//  Copyright © 2019 Enocta. All rights reserved.
+//
+
+import UIKit
+
+class DetailViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
